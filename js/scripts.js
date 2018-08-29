d5334c7cee59663776e01e9878098dcad56088b5
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-08-29 09:55:57 +0300

    add the front-end

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-var slideIndex = 0;
-showSlides();
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000);
-}
+// See the Getting Started docs for more information:
+// http://getbootstrap.com/getting-started/#support-ie10-width
 
+(function () {
+  'use strict';
 
-$('#fullpage').fullpage();
-FastClick.attach(document.body);
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.querySelector('head').appendChild(msViewportStyle)
+  }
+
+})();
